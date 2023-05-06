396752fc987a3c7b6e5ef80c8c67ef0b4608c579
AntonFritsche <antonfritsche1@gmail.com>
2023-05-06 17:47:28 +0000

    WebCrawler fertig + JSON formatiert

--- WebCrawlerScrapy/WebCrawler.py
+++ WebCrawlerScrapy/WebCrawler.py
@@ -2,8 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import datetime
+import os
 
-urls = "https://en.wikipedia.org/wiki/OpenAI"
+urls = [
+    search_item_list
+]
+
+searched_keyword = "Brexit"
+searched_keyword_string = "Suchanfrage:"
 
 def get_page(url):
     response = requests.get(url)
@@ -14,41 +20,49 @@ def get_page(url):
         return None
 
 
-def extract_long_texts(html):
+def get_data(html, searcherd_term_key, searcherd_term_string):
     soup = BeautifulSoup(html, 'html.parser')
     paragraphs = soup.find_all('p')
     long_texts = [str(p.get_text().strip()) for p in paragraphs if len(p.get_text().split()) > 15]
     for text in long_texts:
         print(text)
 
+    number_search = 5
+
     data = {
-        'timestamp': str(datetime.datetime.now()),
-        'long_texts': long_texts
+        f'Search {number_search}' :
+        {
+            searcherd_term_string : searcherd_term_key,
+            'timestamp': str(datetime.datetime.now()),
+            'long_texts': long_texts
+        }
     }
 
     with open('data.json', 'r') as file:
         existing_data = json.load(file)
 
     with open('data.json', 'w') as file:
-        json.dump(long_texts, file, indent=4)
+        json.dump(data, file, indent=5) 
 
-    import os
 
-    file_path = 'data.json'  # Passe den Pfad entsprechend an
-
-    if os.access(file_path, os.W_OK):
+def check_Availability(file_path_Element):
+    if os.access(file_path_Element, os.W_OK):
         print("Sie haben Schreibberechtigungen für den angegebenen Pfad.")
     else:
         ("Sie haben keine Schreibberechtigungen für den angegebenen Pfad.")
 
-def run_crawler(urls):
+check_Availability('data.json')
+
+def run_crawler(urls, searched_keyword, searched_keyword_string):
     html = get_page(urls)
     if html is None:
         print("Fehler beim Abrufen der Startseite.")
         return
-    page_text = extract_long_texts(html)
+    page_text = get_data(html, searched_keyword, searched_keyword_string)
     if page_text != None:
         print('Es funktioniert...')
     else: print('Kein Inhalt mehr... ')
 
-run_crawler(urls)
+for url in urls:
+    for lol in url:
+        run_crawler(url, searched_keyword, searched_keyword_string)
