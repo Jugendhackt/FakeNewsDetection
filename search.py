d3586c2a0ec52951e9f74bc68bebc4c88e10d622
Paul <storrpaul1@gmail.com>
2023-05-07 09:28:58 +0200

    update

--- search.py
+++ search.py
@@ -2,10 +2,11 @@ from googlesearch import search
 from datetime import datetime, timedelta
 import numpy as np
 
+
 def search_(text):
     date = datetime.today() - timedelta(days=7)
     date = 'after:'+np.array(str(date).split(' '))[0]
-    query = text+' news '+str(date)
+    query = text+' news-article '+str(date)
     lst = []
     for j in search(query, stop=10, lang = 'en'):
         lst.append(j)
